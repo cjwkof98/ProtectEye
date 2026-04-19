fdeb6be62c3bbedd57ec01e551238389c49bcbab
chenjw <18950296380@qq.com>
2026-04-19 22:25:48 +0800

    feat: replace blurry icon with HD logo and bump version to v0.3.2

--- AppVersion.cs
+++ AppVersion.cs
@@ -2,7 +2,5 @@ namespace ProtectEye;
 
 public static class AppVersion
 {
-    public const string Version = "v0.3.0";
-    public const string AppName = "ProtectEye";
-    public const string FullName = $"{AppName} {Version}";
+    public const string Version = "v0.3.2";
 }
